a0c436da7e8552644c652cb698e57403de9a50f0
mhmmdrafii <81939949+mhmmdrafii20@users.noreply.github.com>
2026-06-28 05:03:53 +0700

    Update PdfServices.ts

--- app/services/pdf/PdfServices.ts
+++ app/services/pdf/PdfServices.ts
@@ -2,7 +2,13 @@ import puppeteer from 'puppeteer';
 
 export class PdfServices {
     static async generatePdf(html: string) {
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+            ],
+        });
         const page = await browser.newPage();
 
         await page.setContent(html, { waitUntil: 'networkidle0' });
