3ae33b3c41be7fa732ceeab094d0223caeb3d24f
mhmmdrafii <81939949+mhmmdrafii20@users.noreply.github.com>
2026-06-28 05:30:40 +0700

    Update PdfServices.ts

--- app/services/pdf/PdfServices.ts
+++ app/services/pdf/PdfServices.ts
@@ -1,10 +1,19 @@
 import puppeteer from 'puppeteer';
+import { execSync } from 'node:child_process'
 
 export class PdfServices {
     static async generatePdf(html: string) {
+        try {
+            console.log('=== FIND CHROMIUM ===')
+            console.log(execSync('find / -name chromium 2>/dev/null || true').toString())
+            console.log(execSync('find / -name chrome 2>/dev/null || true').toString())
+        } catch (e) {
+        console.error(e)
+        }
+
         const browser = await puppeteer.launch({
             headless: true,
-            executablePath: '/usr/bin/chromium-browser', 
+            executablePath: '/opt/google/chrome/chrome', 
             args: [
                 '--no-sandbox',
                 '--disable-setuid-sandbox',
