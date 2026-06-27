57f523d5352e7a74b1ced23371c94ed425ebe141
mhmmdrafii <81939949+mhmmdrafii20@users.noreply.github.com>
2026-06-28 05:18:16 +0700

    Update PdfServices.ts

--- app/services/pdf/PdfServices.ts
+++ app/services/pdf/PdfServices.ts
@@ -4,6 +4,7 @@ export class PdfServices {
     static async generatePdf(html: string) {
         const browser = await puppeteer.launch({
             headless: true,
+            executablePath: '/usr/bin/google-chrome', 
             args: [
                 '--no-sandbox',
                 '--disable-setuid-sandbox',
