5b1e7560965c444a3226d6cf489d2dc4e66b7c62
mhmmdrafii <81939949+mhmmdrafii20@users.noreply.github.com>
2026-06-28 06:43:14 +0700

    fix puppeteer

--- app/services/pdf/PdfServices.ts
+++ app/services/pdf/PdfServices.ts
@@ -3,7 +3,6 @@ import puppeteer from 'puppeteer';
 export class PdfServices {
     static async generatePdf(html: string) {
         const browser = await puppeteer.launch({
-            executablePath: "/usr/bin/chromium",
             headless: true,
             args: [
                 '--no-sandbox',
